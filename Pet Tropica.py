ea973b03943b9c619135d2b4d72e1edde10e7058
Katie Yip <56652688+Katie-Yip@users.noreply.github.com>
2025-02-15 15:57:47 -0500

    Update Pet Tropica.py

--- Pet Tropica.py
+++ Pet Tropica.py
@@ -1,19 +1,16 @@
 """
-Pygame base template for opening a window
-
-Sample Python/Pygame Programs
-Simpson College Computer Science
-http://programarcadegames.com/
-http://simpson.edu/computer-science/
-
-Explanation video: http://youtu.be/vRB_983kUMc
+ Pygame base template for opening a window
+ 
+ Sample Python/Pygame Programs
+ Simpson College Computer Science
+ http://programarcadegames.com/
+ http://simpson.edu/computer-science/
+ 
+ Explanation video: http://youtu.be/vRB_983kUMc
 """
-
+ 
 import pygame
-import happinessbar
-import clock
-import datetime
-
+ 
 # Define some colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -22,11 +19,10 @@ RED = (255, 0, 0)
 LIGHT_BLUE = (136, 200, 255)
 DARK_BLUE = (88, 177, 255)
 BROWN_FLR = (92, 64, 51)
-
+ 
 pygame.init()
 
 def baseBG():
-#Background
     pygame.draw.rect(screen, DARK_BLUE, [0, 0, 40, 640])
     pygame.draw.rect(screen, DARK_BLUE, [80, 0, 40, 640])
     pygame.draw.rect(screen, DARK_BLUE, [160, 0, 40, 640])
@@ -43,46 +39,43 @@ def baseBG():
     #Info Slot
     pygame.draw.rect(screen, WHITE, [0, 400, 480, 240])
 
+ 
 # Set the width and height of the screen [width, height]
 size = (480, 640)
 screen = pygame.display.set_mode(size)
-
+ 
 pygame.display.set_caption("Pet Tropica")
-
+ 
 # Loop until the user clicks the close button.
 done = False
-
+ 
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
-
+ 
 # -------- Main Program Loop -----------
 while not done:
-# --- Main event loop
+    # --- Main event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-
-# --- Game logic should go here
-
-
-
-# --- Screen-clearing code goes here
-
-# Here, we clear the screen to white. Don't put other drawing commands
-# above this, or they will be erased with this command.
-
-# If you want a background image, replace this clear with blit'ing the
-# background image.
-screen.fill(LIGHT_BLUE)
-
-# --- Drawing code should go here
-baseBG()
-
-
-# --- Go ahead and update the screen with what we've drawn.
-pygame.display.flip()
-
-# --- Limit to 60 frames per second
-clock.tick(60)
-
+ 
+    # --- Game logic should go here
+ 
+    # --- Screen-clearing code goes here
+ 
+    # Here, we clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+ 
+    # If you want a background image, replace this clear with blit'ing the
+    # background image.
+    screen.fill(LIGHT_BLUE)
+ 
+    # --- Drawing code should go here
+    baseBG()
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+ 
 # Close the window and quit.
