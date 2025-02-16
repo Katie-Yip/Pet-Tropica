db28f5430089fa3db0623e41207e1a461e2cda1d
Naz Serrano <serranonazjohann@gmail.com>
2025-02-16 09:16:34 -0500

    resized cat images

--- home.py
+++ home.py
@@ -1,6 +1,5 @@
 import pygame
 import dropdown
-import update
 
 # Define some colors
 BLACK = (0, 0, 0)
@@ -15,11 +14,11 @@ LIGHT_BROWN_CLT = (119, 82, 64)
 CREAM = (255, 253, 208)
 
 #FOR DROPDOWN
-options = ["thing1","thing2","thing3"]
+options = ["thing1","thing2","thing3","thing4","thing5","thing6","thing7","thing8","thing9","thing10"]
 modes = ["easy","medium","hard"]  
-happy = True
+sad = True
 
-def baseBG(screen,mouse_clicked):
+def baseBG(screen):
     pygame.draw.rect(screen, DARK_BLUE, [0, 0, 40, 640])
     pygame.draw.rect(screen, DARK_BLUE, [80, 0, 40, 640])
     pygame.draw.rect(screen, DARK_BLUE, [160, 0, 40, 640])
@@ -37,12 +36,12 @@ def baseBG(screen,mouse_clicked):
     pygame.draw.rect(screen, WHITE, [0, 400, 480, 240])
 
     #CAT
-    if happy:
-        cat = pygame.image.load("Images/basecat.png")
-        resized_cat = pygame.transform.scale(cat, (300, 250))
-        screen.blit(resized_cat, (105, 150))
+    cat = pygame.image.load("Images/basecat.png")
+    resized_cat = pygame.transform.scale(cat, (300, 250))
+    screen.blit(resized_cat, (105, 150))
     tiredcat = pygame.image.load("Images/tiredcat.png")
     resized_tiredcat = pygame.transform.scale(tiredcat, (300, 250))
+
 
     #for font
     font = pygame.font.SysFont('georgia', 18)
@@ -58,13 +57,19 @@ def baseBG(screen,mouse_clicked):
     screen.blit(text2, text2_position)
     screen.blit(text3, text3_position)
 
-    todo(screen,mouse_clicked)
-    updating(screen,)
-
 def popup(screen, popup_visible, accessory):
     #SUNHAT
-    sunhat = pygame.image.load("Images/sunhatcat.png")
-    resized_sunhat = pygame.transform.scale(sunhat, (250, 150))
+    chefhat_image = pygame.image.load("Images/chefhat.png")
+    fancy_image = pygame.image.load("Images/fancy.png")
+    leaf_image = pygame.image.load("Images/leaf.png")
+    sunhat_image = pygame.image.load("Images/sunhat.png")
+    tophat_image = pygame.image.load("Images/tophat.png")
+
+    resized_sunhat = pygame.transform.scale(sunhat_image, (250, 150))
+    resized_chefhat = pygame.transform.scale(chefhat_image, (250, 150))
+    resized_fancy = pygame.transform.scale(fancy_image, (250,150))
+    resized_leaf = pygame.transform.scale(leaf_image,(250, 150))
+    resized_tophat = pygame.transform.scale(tophat_image,(250,150))
 
     #BUTTON
     font = pygame.font.Font(None, 36)
@@ -91,49 +96,39 @@ def popup(screen, popup_visible, accessory):
 
         if accessory == 0:
             screen.blit(resized_sunhat, (130, 250))
+        #if accessory == 1:
 
-def todo(screen,mouse_clicked):
-    mouse_x, mouse_y = pygame.mouse.get_pos()
+        
 
-    todobutton = dropdown.Dropdown(20,450,120,30,options) 
-    todobutton2 = dropdown.Dropdown(20,490,120,30,options) 
-    todobutton3 = dropdown.Dropdown(20,530,120,30,options) 
+def todo(screen):
+    todobutton = dropdown.Dropdown(20,440,120,30,options) 
+    todobutton2 = dropdown.Dropdown(20,500,120,30,options) 
+    todobutton3 = dropdown.Dropdown(20,560,120,30,options) 
 
-    level = dropdown.Dropdown(170,450,140,30,modes) 
-    level2 = dropdown.Dropdown(170,490,140,30,modes) 
-    level3 = dropdown.Dropdown(170,530,140,30,modes) 
+    level = dropdown.Dropdown(170,440,140,30,modes) 
+    level2 = dropdown.Dropdown(170,500,140,30,modes) 
+    level3 = dropdown.Dropdown(170,560,140,30,modes) 
+    """
+    #for font
+    font = pygame.font.SysFont('georgia', 18)
+    text1 = font.render("To Do", True, (0,0,0))
+    text2 = font.render("Level", True, (0,0,0))
+    text3 = font.render("Update", True, (0,0,0))
+
+    text1_position = (50,410)
+    text2_position = (220,410)
+    text3_position= (380,410)
+    """
 
     #TO DO buttons
-    todobutton3.draw(screen,mouse_x,mouse_y)
-    todobutton2.draw(screen,mouse_x,mouse_y)
-    todobutton.draw(screen,mouse_x,mouse_y)
+    todobutton2.draw(screen)
+    todobutton3.draw(screen)
+    todobutton.draw(screen)
     #mode button
-    level3.draw(screen,mouse_x,mouse_y) 
-    level2.draw(screen,mouse_x,mouse_y)
-    level.draw(screen,mouse_x,mouse_y)
+    level.draw(screen)
+    level2.draw(screen)
+    level3.draw(screen)
 
-    if(mouse_clicked):
-        todobutton3.check_click(mouse_x,mouse_y)
-        todobutton2.check_click(mouse_x,mouse_y)
-        todobutton.check_click(mouse_x,mouse_y)
-
-        level3.check_click(mouse_x,mouse_y) 
-        level2.check_click(mouse_x,mouse_y)
-        level.check_click(mouse_x,mouse_y)
-
-def updating(screen):
-    font = pygame.font.Font(None, 36)
-    accept=update.Update(330,450,140,30,GREEN,"Accept",font,WHITE)
-    accept2=update.Update(330,490,140,30,GREEN,"Accept",font,WHITE)
-    accept3=update.Update(330,530,140,30,GREEN,"Accept",font,WHITE)
-
-    accept.draw(screen)
-    accept2.draw(screen)
-    accept3.draw(screen)
-
-    if accept.is_clicked():
-        accept=update.Update(330,450,140,30,RED,"Accept",font,WHITE)
-
-
-  
-
+    """screen.blit(text1, text1_position)
+    screen.blit(text2, text2_position)
+    screen.blit(text3, text3_position)"""
